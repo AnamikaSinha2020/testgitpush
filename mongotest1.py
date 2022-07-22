afcc73ca569788ceb7ae3f80414b8a73ddd5b18f
Manish Sinha <manishkumarsinha@gmail.com>
2022-07-21 22:35:37 -0400

    New file added to the project

---
+++ mongotest1.py
@@ -0,0 +1,12 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://ineuron:mongodb123:@cluster0.33ckn1h.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+test
+test
+dsff
+df
+
+adff
+df
+ds
